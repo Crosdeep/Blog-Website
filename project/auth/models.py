e7cdd06f2ccdd37bede7b2e904fa5af6113c1e0b
Crosdeep <pinarpinar959@mail.com>
2024-11-27 12:37:53 +0300

    auth

--- project/auth/models.py
+++ project/auth/models.py
@@ -2,11 +2,11 @@ from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from datetime import datetime
-
-
+from project.blog.models import BlogPost
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -14,10 +14,9 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)  #Kullanıcının oluşturulma tarihi
-    #role = db.Column(db.String(50), nullable=False, default="user")  # admin ya da user olarak ayarlandı
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Kullanıcının oluşturulma tarihi
 
-
+    # role = db.Column(db.String(50), nullable=False, default="user")  # admin ya da user olarak ayarlandı
 
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
@@ -28,3 +27,7 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role_id = db.Column(db.Integer, unique=True)
